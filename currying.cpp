a7a3ced0ab9f4bd0a811b29180874edd2d8d4fc8
yx-z <yx-z@outlook.com>
2023-06-28 20:11:50 -0400

    Add currying

--- currying.cpp
+++ currying.cpp
@@ -1,67 +1,43 @@
 #include <type_traits>
-
-template<typename, typename = void>
-struct self_invokable : std::false_type {
-};
-
-template<typename T>
-struct self_invokable<T, std::void_t<std::invoke_result_t<T>>> : std::true_type {
-};
+#include <functional>
+#include <iostream>
 
 template<typename Func>
 constexpr auto currying(Func&& func) {
-    if constexpr (self_invokable<Func>::value) {
+    if constexpr (std::is_invocable_v<Func>) {
         // at the last step, we have to produce result instead of returning new functions
         return func();
     } else {
         return [func](auto&& arg) {
             return currying(
-                    [func, arg](auto&& ...args) -> decltype(func(arg, args...)) {
-                        return func(arg, args...);
-                    }
+                    [func, arg](auto&& ...args) -> decltype(func(arg, args...)) { return func(arg, args...); }
             );
         };
     }
 }
 
-
 constexpr static auto addThenMult = [](int i1, int i2, int i3) { return (i1 + i2) * i3; };
 constexpr static auto res = currying(addThenMult)(1)(2)(3);
 static_assert(res == 9);
 
-
-// separate implementation involving <tuple>
+// separate implementation involving std::function
 // curry of zero-and-one-arg functions are trivial: curry(f) = f for such functions.
 // hence their specializations are ignored.
-// TODO: make below constexpr and better template argument deductible?
-
-#include <functional>
-#include <tuple>
-#include <iostream>
-
-template <typename Ret, typename Arg1, typename Arg2, typename... Args>
-auto curry(std::function<Ret(std::tuple<Arg1, Arg2, Args...>)> f) {
+template<typename Ret, typename Arg, typename... Args>
+auto curry(std::function<Ret(Arg, Args...)> f) {
     if constexpr (sizeof...(Args) == 0) {
-        return [=](Arg1 arg1) { return [=](Arg2 arg2) { return f(std::tuple{arg1, arg2}); }; };
+        return f;
     } else {
-        return [=](Arg1 arg1) {
-            return curry(std::function{[=](std::tuple<Arg2, Args...> tup) { 
-                return std::apply([=](auto... args){ return f(std::tuple{arg1, args...}); }, tup);
-            }});
+        return [f](Arg arg) {
+            return curry(std::function{[f, arg](Args... args) { return f(arg, args...); }});
         };
     }
 }
 
-const static auto two_args = curry(
-    std::function([](std::tuple<int, int> pair) { return std::get<0>(pair) + std::get<1>(pair); })
-);
-const static auto three_args = curry(
-    std::function([](std::tuple<int, int, int> triple) {
-        return std::get<0>(triple) * std::get<1>(triple) * std::get<2>(triple); })
-);
-
 int main() {
+    const static auto two_args = curry(std::function{[](int i1, int i2) { return i1 + i2; }});
+    const static auto three_args = curry(std::function{[](int i1, int i2, int i3) { return i1 + i2 * i3; }});
     std::cout << two_args(1)(2) << std::endl; // 3
-    std::cout << three_args(1)(2)(3) << std::endl; // 6
+    std::cout << three_args(1)(2)(3) << std::endl; // 7
     return 0;
 }
